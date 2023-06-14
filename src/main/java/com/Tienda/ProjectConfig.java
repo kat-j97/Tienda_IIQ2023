b4e0e0f8c6e899dcc2150ed515f617ef728eaf22
kat-j97 <kjarquin.fonseca06@gmail.com>
2023-06-13 21:44:10 -0600

    Modificación de Semana 4

---
+++ src/main/java/com/Tienda/ProjectConfig.java
@@ -0,0 +1,49 @@
+package com.Tienda;
+
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration 
+public class ProjectConfig implements WebMvcConfigurer{
+    /* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto */
+
+    /* localeResolver se utiliza para crear una sesión de cambio de idioma */
+    @Bean
+    public SessionLocaleResolver localeResolver() { //Maneja internamente cual es la localizacion que tenemos nosotros
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault()); //Maquina
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+
+        return slr;
+    }
+
+    /* localeChangeInterceptor se utiliza para crear un interceptor de cambio de idioma */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() { //atento a cuando cambien el lenguaje
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    //Bean para poder acceder a los Messages.properties en código...
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
